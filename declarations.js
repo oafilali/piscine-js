81491b33b0fd3def27e2f643bc23d59d933c4d99
oafilali <medafilali@gmail.com>
2025-02-10 15:09:01 +0200

    Add declarations.js with initial variable and object definitions

---
+++ declarations.js
@@ -0,0 +1,16 @@
+const escapeStr = "let's go"
+const arr = [4, '2']
+const obj = {
+    str : "hello",
+    num : 12,
+    bool : true,
+    undef : undefined
+}
+const nested = {
+    arr : [4, undefined, '2'],
+    obj : {
+        str : "there",
+        num : 24,
+        bool : true
+    }
+}
