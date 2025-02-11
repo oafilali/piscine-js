9b5cfab8dc70d1deb90c36e56d4f87533a864f48
oafilali <medafilali@gmail.com>
2025-02-11 12:00:54 +0200

    Fix formatting issues in mutability.js by adding missing semicolons

--- mutability.js
+++ mutability.js
@@ -1,7 +1,7 @@
 
-const  sameperson = person
-const clone1 = {...person}
-const clone2 = {...person}
+const  sameperson = person;
+const clone1 = {...person};
+const clone2 = {...person};
 
 person.age = 78
 person.country = 'FR'
