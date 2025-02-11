d93a2f5ba59fd4106a71308c3aecd373757292c9
oafilali <medafilali@gmail.com>
2025-02-11 13:08:35 +0200

    Refactor dogYears function to improve readability and ensure result is rounded to two decimal places

--- dog-years.js
+++ dog-years.js
@@ -1,16 +1,17 @@
 const orbits = {
-    earth : 1,
-    mercury : 0.2408467,
-    venus : 0.61519726,
-    mars : 1.8808158,
-    jupiter : 11.862615,
-    saturn : 29.447498,
-    uranus : 84.016846,
-    neptune : 164.79132
+    earth: 1,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132
 }
 
 const dogYears = (planet, age) => {
-    return parseFloat(((age / 31557600) *  orbits[planet]).toFixed(2) * 7)
+    const result = ((age / 31557600) * orbits[planet]) * 7;
+    return Math.round(result * 100) / 100; 
 }
 
-console.log(dogYears('earth', 1000000000), 221.82)
+console.log(dogYears('earth', 1000000000))
