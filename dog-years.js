66d7996c9c724c474209709759b26793686708b9
oafilali <medafilali@gmail.com>
2025-02-11 12:42:36 +0200

    Add dog-years.js to calculate dog age in planetary years

---
+++ dog-years.js
@@ -0,0 +1,14 @@
+orbits = {
+    earth : 1,
+    mercury : 0.2408467,
+    venus : 0.61519726,
+    mars : 1.8808158,
+    jupiter : 11.862615,
+    saturn : 29.447498,
+    uranus : 84.016846,
+    neptune : 164.79132
+}
+
+const dogYears = (planet, age) => {
+    ((age / 31,557,600) /  orbits[planet]) * 7
+}
