8c4640cb3b9acf7f0cec72b910a2b67b855ad686
oafilali <medafilali@gmail.com>
2025-02-12 15:00:01 +0200

    Fix slice function to correctly handle negative indices and undefined endIndex

--- slicer.js
+++ slicer.js
@@ -1,7 +1,13 @@
 const slice = (arg, startIndex, endIndex) => {
-    startIndex = startIndex || 0;
-    endIndex = endIndex === undefined ? arg.length : endIndex;
-
+    if (endIndex === undefined) {
+        endIndex = input.length;
+    }
+    if (startIndex < 0) {
+        startIndex = input.length + startIndex;
+    }
+    if (endIndex < 0) {
+        endIndex = input.length + endIndex;
+    }
     if (Array.isArray(arg)) {
         let result = [];
         for (let i = startIndex; i < endIndex; i++) {
