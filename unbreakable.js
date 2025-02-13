458364b22c4c760eb4dbc99e74916f39c7e8fda1
oafilali <medafilali@gmail.com>
2025-02-13 22:21:59 +0200

    Refactor split and join functions for improved readability and efficiency

--- unbreakable.js
+++ unbreakable.js
@@ -1,27 +1,25 @@
 const split = (arg, separator) => {
-    let subStr = "";
-    let result = [];
-    let sepLen = separator.length;
+    let subStr = ""
+    let result = []
     for (let i = 0; i < arg.length; i++) {
-        if (arg.slice(i, i + sepLen) === separator) {
+        if (arg[i] !== separator) {
+            subStr += arg[i]
+        } else {
             result.push(subStr);
             subStr = "";
-            i += sepLen - 1;
-        } else {
-            subStr += arg[i];
         }
     }
-    result.push(subStr);
-    return result;
+    result.push(subStr)
+    return result
 }
 
 const join = (arg, joiner) => {
-    let result = "";
+    let result = ""
     for (let i = 0; i < arg.length; i++) {
-        result += arg[i];
-        if (i < arg.length - 1) {
-            result += joiner;
+        result += arg[i]
+        if (i < arg.length -1) {
+            result += joiner
         }
     }
-    return result;
+    return result
 }
