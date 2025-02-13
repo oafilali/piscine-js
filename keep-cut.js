a892ec91fcec444ff9c97e124ebc6d4b295121cc
oafilali <medafilali@gmail.com>
2025-02-13 12:09:44 +0200

    Refactor keepFirstLast function to handle strings with length less than 4

--- keep-cut.js
+++ keep-cut.js
@@ -3,4 +3,9 @@ const cutLast = (str) => str.slice(0, str.length - 2);
 const cutFirstLast = (str) => str.slice(2, str.length - 2);
 const keepFirst = (str) => str.slice(0, 2);
 const keepLast = (str) => str.slice(str.length - 2);
-const keepFirstLast = (str) => str.slice(0, 2) + str.slice(str.length - 2);
+const keepFirstLast = (str) => {
+    if (str.length >= 4) {
+        str.slice(0, 2) + str.slice(str.length - 2);
+    }
+    return str
+}
