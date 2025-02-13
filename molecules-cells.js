6eb1cdd2880c9a6e00fe2abf089d8e65e3d3b823
oafilali <medafilali@gmail.com>
2025-02-13 23:10:52 +0200

    Implement RNA function to transcribe DNA sequences into RNA

--- molecules-cells.js
+++ molecules-cells.js
@@ -1,3 +1,15 @@
 const RNA = (str) => {
-    
+    let result = ""
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "G") {
+            result += "C"
+        } else if (str[i] === "C") {
+            result += "G"
+        } else if (str[i] === "T") {
+            result += "A"
+        } else if (str[i] === "A") {
+            result += "U"
+        }
+    }
+    return result
 }
