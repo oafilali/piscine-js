6cb850dcf8002dbb1f5f7d2ffb2142aa2fffc94b
oafilali <medafilali@gmail.com>
2025-02-13 15:21:05 +0200

    Enhance reverse function to handle both arrays and strings, returning the reversed result for each type

--- reverser.js
+++ reverser.js
@@ -1,7 +1,15 @@
 const reverse = (arr) => {
-    let reservedArr = []
-    for (let i = arr.length - 1; i >= 0; i--) {
-        reservedArr.push(arr[i])
+    if (Array.isArray(arr)) {
+        let reversedArr = [];
+        for (let i = arr.length - 1; i >= 0; i--) {
+            reversedArr.push(arr[i]);
+        }
+        return reversedArr;
+    } else if (typeof arr === "string") {
+        let reversedStr = ""
+        for (let i = arr.length - 1; i >= 0; i--) {
+            reversedStr += arr[i]
+        }
+        return reversedStr
     }
-    return reservedArr
 }
