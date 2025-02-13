0c24b8f027e2bbd7cdcdd95b6a84b8c7bcb01edb
oafilali <medafilali@gmail.com>
2025-02-13 15:02:04 +0200

    Refactor rounding functions for improved logic and clarity

--- cut-corners.js
+++ cut-corners.js
@@ -1,64 +1,86 @@
-const trunc = (num) => {
+function round(int) {
     let neg = false;
-    if (num < 0) {
+    if (int < 0) {
         neg = true;
-        num = -num;
+        int = -int;
     }
-    let intCopy = num;
     let counter = 0;
-    while (intCopy >= 1) {
-        intCopy -= 1;
+    while (!(int < 1 && int > -1)) {
+        int -= 1;
         counter++;
     }
-    return neg ? -counter : counter;
-}
-
-const round = (num) => {
-    let neg = false;
-    if (num < 0) {
-        neg = true;
-        num = -num;
-    }
-    let intCopy = num;
-    let counter = 0;
-    while (intCopy >= 1) {
-        intCopy -= 1;
-        counter++;
-    }
-    if (intCopy < 0.5) {
-        return neg ? -counter : counter;
+    if (int < 0.5) {
+        if (neg) {
+            return -counter;
+        } else {
+            return counter;
+        }
     } else {
-        return neg ? -counter - 1 : counter + 1;
+        if (neg) {
+            return -counter - 1;
+        } else {
+            return counter + 1;
+        }
     }
 }
 
-const ceil = (num) => {
-    if (num === 0) return 0;
+function floor(int) {
     let neg = false;
-    if (num < 0) {
+    if (int < 0) {
         neg = true;
-        num = -num;
+        int = -int;
     }
-    let intCopy = num;
+    let intCopy = int;
     let counter = 0;
-    while (intCopy >= 1) {
+    while (!(intCopy < 1 && intCopy > -1)) {
         intCopy -= 1;
         counter++;
     }
-    return neg ? -counter : counter + 1;
+    if (neg) {
+        return -counter - 1;
+    } else {
+        return counter;
+    }
 }
 
-const floor = (num) => {
+function ceil(int) {
+    if (!int) return 0;
     let neg = false;
-    if (num < 0) {
+    if (int < 0) {
         neg = true;
-        num = -num;
+        int = -int;
     }
-    let intCopy = num;
+    let intCopy = int;
     let counter = 0;
-    while (intCopy >= 1) {
+    while (!(intCopy < 1 && intCopy >= 0)) {
         intCopy -= 1;
         counter++;
     }
-    return neg ? -counter - 1 : counter;
+    if (neg) {
+        return -counter;
+    } else {
+        return counter + 1;
+    }
+}
+
+function trunc(int) {
+    let counter = 0;
+    if (int > 0xfffffffff) {
+        int -= 0xfffffffff;
+        counter += 0xfffffffff;
+    }
+    let neg = false;
+    if (int < 0) {
+        neg = true;
+        int = -int;
+    }
+    let intCopy = int;
+    while (!(intCopy < 1 && intCopy > -1)) {
+        intCopy -= 1;
+        counter++;
+    }
+    if (neg) {
+        return -counter;
+    }
+    return counter;
 }
