a1642588600ec719578f638e6dff3a972dcbe196
oafilali <medafilali@gmail.com>
2025-02-13 15:06:50 +0200

    Refactor rounding functions for improved logic and clarity

--- cut-corners.js
+++ cut-corners.js
@@ -1,86 +1,33 @@
-function round(int) {
-    let neg = false;
-    if (int < 0) {
-        neg = true;
-        int = -int;
-    }
+const trunc = (num) => {
     let counter = 0;
-    while (!(int < 1 && int > -1)) {
-        int -= 1;
-        counter++;
-    }
-    if (int < 0.5) {
-        if (neg) {
-            return -counter;
-        } else {
-            return counter;
-        }
-    } else {
-        if (neg) {
-            return -counter - 1;
-        } else {
-            return counter + 1;
-        }
-    }
-}
-
-function floor(int) {
-    let neg = false;
-    if (int < 0) {
-        neg = true;
-        int = -int;
-    }
-    let intCopy = int;
-    let counter = 0;
-    while (!(intCopy < 1 && intCopy > -1)) {
-        intCopy -= 1;
-        counter++;
-    }
-    if (neg) {
-        return -counter - 1;
-    } else {
-        return counter;
-    }
-}
-
-function ceil(int) {
-    if (!int) return 0;
-    let neg = false;
-    if (int < 0) {
-        neg = true;
-        int = -int;
-    }
-    let intCopy = int;
-    let counter = 0;
-    while (!(intCopy < 1 && intCopy >= 0)) {
-        intCopy -= 1;
-        counter++;
-    }
-    if (neg) {
-        return -counter;
-    } else {
-        return counter + 1;
-    }
-}
-
-function trunc(int) {
-    let counter = 0;
-    if (int > 0xfffffffff) {
-        int -= 0xfffffffff;
+    if (num > 0xfffffffff) {
+        num -= 0xfffffffff;
         counter += 0xfffffffff;
     }
     let neg = false;
-    if (int < 0) {
+    if (num < 0) {
         neg = true;
-        int = -int;
+        num = -num;
     }
-    let intCopy = int;
-    while (!(intCopy < 1 && intCopy > -1)) {
+    let intCopy = num;
+    while (intCopy >= 1) {
         intCopy -= 1;
         counter++;
     }
-    if (neg) {
-        return -counter;
-    }
-    return counter;
+    return neg ? -counter : counter;
+}
+
+const round = (num) => {
+    let rounded = num + (num < 0 ? -0.5 : 0.5);
+    return trunc(rounded);
+}
+
+const ceil = (num) => {
+    const int = trunc(num);
+    return int < num ? int + 1 : int;
+}
+
+const floor = (num) => {
+    const int = trunc(num);
+    return int > num ? int - 1 : int;
 }
