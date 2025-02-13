55398809636124fe011fbf3117deb04fa4db665b
oafilali <medafilali@gmail.com>
2025-02-13 23:05:03 +0200

    Refactor nasa function to generate sequence with correct handling for multiples of 3 and 5

--- nasa.js
+++ nasa.js
@@ -1,20 +1,20 @@
 const nasa = (N) => {
-    let result = ""
-    if (N % 3 === 0) {
-        return "NA"
-    } else if (N % 5 === 0) {
-        return "SA"
-    } else if (N % 3 === 0 && N % 5 === 0) {
-        return "NASA"
-    } else {
-        for (let i = 1; i <= N; i++) {
-            result += String(i)
-            if ( i !== N) {
-                result += " "
-            }
+    let result = "";
+    for (let i = 1; i <= N; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            result += "NASA";
+        } else if (i % 3 === 0) {
+            result += "NA";
+        } else if (i % 5 === 0) {
+            result += "SA";
+        } else {
+            result += String(i);
         }
-        return result
+        if (i !== N) {
+            result += " ";
+        }
     }
+    return result;
 }
 
-console.log(nasa(11))
+console.log(nasa(15));
