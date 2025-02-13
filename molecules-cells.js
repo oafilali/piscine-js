5c7c417215382e8d85f27bb10b37084988aaaabb
oafilali <medafilali@gmail.com>
2025-02-13 23:12:57 +0200

    Add DNA function to transcribe RNA sequences into DNA

--- molecules-cells.js
+++ molecules-cells.js
@@ -13,3 +13,19 @@ const RNA = (str) => {
     }
     return result
 }
+
+const DNA = (str) => {
+    let result = ""
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === "G") {
+            result += "C"
+        } else if (str[i] === "C") {
+            result += "G"
+        } else if (str[i] === "U") {
+            result += "A"
+        } else if (str[i] === "A") {
+            result += "T"
+        }
+    }
+    return result
+}
