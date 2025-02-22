104655a210b2ab27ed730ef36935912d2f819566
oafilali <medafilali@gmail.com>
2025-02-22 21:04:03 +0200

    Refactor pimp function to improve button style toggling logic

--- pimp-my-style.js
+++ pimp-my-style.js
@@ -7,20 +7,20 @@ export const pimp = () => {
     const button = document.querySelector('.button');
 
     if (adding) {
+        button.classList.remove('unpimp');
         button.classList.add(styles[index]);
         index++;
         if (index === styles.length) {
             adding = false;
-            button.classList.toggle('unpimp');
+            button.classList.add('unpimp');
         }
     } else {
+        button.classList.add('unpimp');
         index--;
         button.classList.remove(styles[index]);
         if (index === 0) {
             adding = true;
-            button.classList.toggle('unpimp');
-        } else {
-            button.classList.toggle('unpimp');
+            button.classList.remove('unpimp');
         }
     }
 };
