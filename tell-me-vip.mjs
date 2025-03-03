497401385a515eaa74c1fe7f474433e6c0c5ee06
oafilali <medafilali@gmail.com>
2025-03-03 23:42:05 +0200

    add tellMeWho function to list VIP guests from JSON files

---
+++ tell-me-vip.mjs
@@ -0,0 +1,53 @@
+import fs from "fs/promises"
+
+async function tellMeWho() {
+    let file = await howMany()
+    let vipGuests = []
+    for (const element of file) {
+        let data = await fileReader(`./guests/${element}`)
+        let obj = JSON.parse(data)
+        if (obj.answer.toLowerCase() === "yes") {
+            vipGuests.push(element)
+        }
+    }
+
+    let guests = []
+    vipGuests.forEach(person => {
+        guests.push(removeExtention(person).split("_"))
+    })
+    guests.sort((a, b) => a[1].localeCompare(b[1]))
+    let result = ""
+    let counter = 1
+    for (let i = 0; i < guests.length; i++) {
+        result += `${counter}. ${guests[i][1]} ${guests[i][0]}\n`
+        counter++
+    }
+    return result
+}
+
+async function howMany() {
+    let args = process.argv.slice(2, 3)
+    let file = []
+    try {
+        file = await fs.readdir(args[0])
+    } catch {
+        console.error("Error reading the directory...")
+    }
+    return file
+}
+
+function removeExtention(str) {
+    return str.replace(".json", "")
+}
+
+async function fileReader(file) {
+    let data = ""
+    try {
+        data = await fs.readFile(`${file}`, "utf8")
+    } catch (error) {
+        console.error("Error reading file:", error)
+    }
+    return data
+}
+
+console.log(await tellMeWho())
