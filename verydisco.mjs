52e5c879813e0215537c3b477588e81c81894641
oafilali <medafilali@gmail.com>
2025-03-03 13:33:23 +0200

    refactor verydisco function to sanitize input and improve word processing

--- verydisco.mjs
+++ verydisco.mjs
@@ -1,19 +1,21 @@
-const args = process.argv.slice(2)
-let result = []
 
 function verydisco () {
-    args.forEach(word => {
+    const args = process.argv.slice(2, 3)
+    let sanitisedArgs = []
+    args.forEach(Element => {
+        let splitted = Element.split(" ")
+        for (let i = 0; i < splitted.length; i++)
+            sanitisedArgs.push(splitted[i])
+    })
+    let result = []
+    sanitisedArgs.forEach(word => {
         let middle = Math.ceil( word.length / 2 )
         let part1 = word.slice(0, middle)
         let part2 = word.slice(middle)
         let joined = part2 + part1
         result.push(joined)
-        joined = ""
     })
     return result.join(" ")
 }
-
-
-
 
 console.log(verydisco())
