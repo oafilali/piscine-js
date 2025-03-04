9dcfb5dd1be3f4d397207052e0404f56ca7c7a12
oafilali <medafilali@gmail.com>
2025-03-04 14:08:07 +0200

    feat: implement HTTP server for guest data submission with basic authentication and file writing

---
+++ gatecrashers.mjs
@@ -0,0 +1,79 @@
+import http from "http"
+import fs from "fs/promises"
+
+const server = http.createServer(requestHandler)
+
+const serverFailure = {
+    error : "server failed"
+}
+
+const bestFriends = ["Caleb_Squires", "Tyrique_Dalton", "Rahima_Young"]
+const password = "abracadabra"
+
+function startServer() {
+    server.listen(5000, function() {
+        console.log('Server is listening on port 5000')
+    })
+}
+
+async function requestHandler(req, res) {
+    try {
+        if (req.method === "POST") {
+            const authHeader = req.headers['authorization']
+            if (!authHeader) {
+                res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="Secure Area"' })
+                res.end("Unauthorized")
+                return
+            }
+            const [scheme, credentials] = authHeader.split(" ")
+            if (scheme !== "Basic" || !credentials) {
+                res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="Secure Area"' })
+                res.end("Unauthorized")
+                return
+            }
+            const [user, pass] = credentials.split(":")
+            let guestName = req.url.split("/")
+            for (let friend of bestFriends) {
+                if (friend === user && pass === password) {
+                    let body = ""
+                    req.on("data", chunk => {
+                        body += chunk.toString()
+                    })
+                    req.on("end", async () => {
+                        console.log("Recieved data", body)
+                        res.writeHead(201, { "Content-Type": "application/json" })
+                        let err = await fileWriter(`${guestName[guestName.length - 1]}`, body)
+                        if (err) {
+                            res.writeHead(500, { 'Content-Type': 'application/json' })
+                            res.end(JSON.stringify(serverFailure))
+                        }
+                        res.end(body)
+                    })
+                } else {
+                    res.writeHead(401, { 'WWW-Authenticate': 'Basic realm="Secure Area"' })
+                    res.end("Unauthorized")
+                }
+            }
+        } else {
+            res.writeHead(405, { "Content-Type": "application/json" })
+            res.end(JSON.stringify({ error: "Method not allowed" }))
+        }
+    } catch (error) {
+        res.writeHead(500, { 'Content-Type': 'application/json' })
+        res.end(JSON.stringify(serverFailure))
+    }
+}
+
+async function fileWriter(fileName, content) {
+    let err = null
+    try {
+        await fs.writeFile(`./guests/${fileName}.json`, content)
+        console.log("File created succefully")
+    } catch (error) {
+        console.error("Error writing file:", error)
+        err = error
+    }
+    return err
+}
+
+startServer()
